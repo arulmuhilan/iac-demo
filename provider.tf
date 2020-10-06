c3fae43692ae0aa8f2a686a98543bfd9cd38ba98
arulmuhilan <69975817+arulmuhilan@users.noreply.github.com>
2020-10-06 15:26:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
 provider "aws" {
-  region = "ap-south-1"
+  
+  provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAQRSFVFY7OHBG55H7"
+  secret_key = "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"
+}
+  region = "us-east-1"
 }
